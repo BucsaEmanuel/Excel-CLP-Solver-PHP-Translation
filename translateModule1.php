7dbef45da6d54f59af9b6ccb94f5a54713e51678
emanuel <emanuel.bucsa@somproduct.ro>
2019-08-21 15:56:38 +0300

    created file translateModule1.php, started translating module 1

---
+++ translateModule1.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Attribute VB_Name = "Module1"
+ * Option Explicit
+ */
+
+/**
+ * Const epsilon As Double = 0.0001
+ * @var float
+ */
+const epsilon = 0.0001;
+
+/**
+ * Const column_offset As Long = 11
+ * @var integer
+ */
+const column_offset = 11;
+
+/**
+ * Const width_limit As Double = 300
+ * @var float
+ */
+const width_limit = 300;
+
+/**
+ * Const height_limit As Double = 300
+ * @var float
+ */
+const height_limit = 300;
+
+/**
+ * Const displacement_multiplier As Double = 0.353
+ * @var float
+ */
+const displacement_multiplier = 0.353;
